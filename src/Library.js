12876245d2110509cec0666c683599eeb5b1aaec
smithwebtek <brad@smithwebtek.com>
2018-02-23 12:35:34 -0500

    Apply minor updates to NewBook and Library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,7 @@
 export const Library =
-  [
-    { id: 1, title: 'my bike', subject: 'Sports and leisure' },
-    { id: 2, title: 'our dog', subject: 'Domesticated animals' },
-    { id: 3, title: 'the movie', subject: 'Actors and Entertainment' },
-    { id: 3, title: 'Dim Sum', subject: 'Asian comfort food' },
-  ]
+	[
+		{ id: 1, title: 'my bike', subject: 'Sports and leisure' },
+		{ id: 2, title: 'our dog', subject: 'Domesticated animals' },
+		{ id: 3, title: 'the movie', subject: 'Actors and Entertainment' },
+		{ id: 3, title: 'Dim Sum', subject: 'Asian comfort food' },
+	]
--- src/NewBook.js
+++ src/NewBook.js
@@ -2,49 +2,49 @@ import React, { Component } from 'react'
 import { Library } from './Library'
 
 class NewBook extends Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props)
 
-    this.state = {
-      library: Library,
-      book: {
-        title: '',
-        subject: ''
-      }
-    }
-  }
+		this.state = {
+			library: Library,
+			book: {
+				title: '',
+				subject: ''
+			}
+		}
+	}
 
-  handleChange = (e) => {
-    const { name, value } = e.target
-    this.setState({
-      [name]: value
-    })
-  }
+	handleChange = (e) => {
+		const { name, value } = e.target
+		this.setState({
+			[name]: value
+		})
+	}
 
-  handleSubmit = (data) => {
-    let newBookData = this.state.book
-    let newLibrary = [...this.state.library.concat(newBookData)]
-    this.setState({ libary: newLibrary })
-  }
+	handleSubmit = (data) => {
+		let newBookData = this.state.book
+		let newLibrary = [...this.state.library.concat(newBookData)]
+		this.setState({ libary: newLibrary })
+	}
 
-  render() {
-    return (
-      <form>
-        <input
-          type="text"
-          name="title"
-          placeholder="enter_title"
-          onChange={(e) => this.handleChange(e)} />
-        <input
-          type="text"
-          name="subject"
-          placeholder="enter_subject"
-          onChange={(e) => this.handleChange(e)} />
-        <button
-          name="Submit">Submit</button>
-      </form>
-    )
-  }
+	render() {
+		return (
+			<form>
+				<input
+					type="text"
+					name="title"
+					placeholder="enter_title"
+					onChange={(e) => this.handleChange(e)} />
+				<input
+					type="text"
+					name="subject"
+					placeholder="enter_subject"
+					onChange={(e) => this.handleChange(e)} />
+				<button
+					name="Submit">Submit</button>
+			</form>
+		)
+	}
 }
 
 export default NewBook
